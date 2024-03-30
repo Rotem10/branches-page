b2fca58e3f24ac53aaf1d66841c7ae93f4f94fc9
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2024-03-30 20:40:21 +0300

    url

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const URL = "http://localhost:9000/";
+
 const getData = async () => {
-  return (await axios.get("http://localhost:9000/")).data;
+  return (await axios.get(URL)).data;
 };
 export default getData;
